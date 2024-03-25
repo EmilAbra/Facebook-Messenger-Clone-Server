7a16d74f4519ac2ae798d1f9c4f1bb7117367566
eabra <emab21@student.bth.se>
2024-03-25 11:45:31 +0100

    Add Login Route

--- routes/users.ts
+++ routes/users.ts
@@ -1,4 +1,10 @@
 import { FastifyInstance } from "fastify";
+import { StreamChat } from "stream-chat";
+
+const streamChat = StreamChat.getInstance(
+  process.env.STREAM_API_KEY!,
+  process.env.STREAM_PRIVATE_API_KEY!
+);
 
 export async function userRoutes(app: FastifyInstance) {
   app.post<{ Body: { id: string; name: string; image?: string } }>(
@@ -8,6 +14,31 @@ export async function userRoutes(app: FastifyInstance) {
       if (id == null || id === "" || name == null || name === "") {
         return res.status(400).send();
       }
+
+      const existingUsers = await streamChat.queryUsers({ id });
+      if (existingUsers.users.length > 0) {
+        return res.status(400).send("User ID taken");
+      }
+
+      await streamChat.upsertUser({ id, name, image });
     }
   );
+
+  app.post<{ Body: { id: string } }>("/login", async (req, res) => {
+    const { id } = req.body;
+    if (id == null || id === "") {
+      return res.status(400).send();
+    }
+    const {
+      users: [user],
+    } = await streamChat.queryUsers({ id });
+    if (user == null) return res.status(401).send();
+
+    const token = streamChat.createToken(id);
+
+    return {
+      token,
+      user: { id: user.id, name: user.name, image: user.image },
+    };
+  });
 }
